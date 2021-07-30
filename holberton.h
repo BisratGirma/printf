70cb84343fb4049309842b2b23c64b7f61cd970f
bisrax <bisratx#gmail.com>
2021-07-31 00:42:14 +0300

    updating protoy

--- holberton.h
+++ holberton.h
@@ -14,14 +14,14 @@ typedef struct format_specifiers
 {
 	char *specifier;
 	int (*printer)(va_list arg_list);
-} fs;
+} specy;
 
 int print_R(va_list arg);
 int print_rev(va_list arg);
 int print_STR(va_list arg);
 int print_memory(va_list arg_list);
 int _putchar(char c);
-int parser(const char *format, va_list arg_list);
+int hole(const char *format, va_list arg_list);
 int _printf(const char *format, ...);
 int print_char(va_list);
 int print_string(va_list);
@@ -36,9 +36,9 @@ int _strlen(char *string);
 int intlen(int number);
 int isoctal(char *string);
 int octaltodecimal(char *string);
-int fs_looper(const char *format, const char **formatt, va_list arg_list,
-	      fs format_list[]);
-int format_looper(const char *format, va_list arg_list, fs format_list[]);
+int finalstage(const char *format, const char **formatt, va_list arg_list,
+	      specy format_list[]);
+int loophole(const char *format, va_list arg_list, fs format_list[]);
 int print_unsigned(va_list arg_list);
 
 #endif
