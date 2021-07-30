34cd89c271fc1a1a8c129f6760d5c1f509fe6151
bisrax <bisratx#gmail.com>
2021-07-31 00:54:49 +0300

    desc edit

--- function4.c
+++ function4.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- *_pow - raises the number base to power power
- *@base : the base
- *@power : the power
- *Return: return the answer
+ * _pow - raises the number base to power power
+ * @base : the base
+ * @power : the power
+ *
+ * Return: return the answer
  */
 int _pow(unsigned int base, int power)
 {
